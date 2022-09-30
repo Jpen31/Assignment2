873ac4929765cfdb220ce630031f9f9eeef907ba
Jpen <jp3171@princeton.edu>
2022-09-30 13:39:28 -0400

    changes

--- replace.c
+++ replace.c
@@ -83,7 +83,9 @@ int main(int argc, char *argv[])
    pcTo = argv[2];
 
     while (fgets(acLine, MAX_LINE_SIZE, stdin) != NULL) {
-        
+        *acLine = stdin;
+        stdin++;
+        acLine++;
     }
     uReplaceCount = replaceAndWrite(acLine, pcFrom, pcTo);
     fprintf(stderr, "%lu replacements\n", (unsigned long)uReplaceCount);
