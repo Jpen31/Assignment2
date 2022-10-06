308af17fa26c1860dffb899ad4b2b4f0f2c7410a
Jpen <jp3171@princeton.edu>
2022-10-06 12:42:50 -0400

    bug fixes

--- str.h
+++ str.h
@@ -8,25 +8,25 @@
 
 #include <stdlib.h>
 
-/* Returns length of string str. Returns error if str point to NULL. */
+/* Returns length of string str. Str cannot be null. */
 size_t Str_getLength(const char *str);
 
 /* Copies string src to string dest and returns pointer to dest. 
-Returns error if dest and src  point to null */
+dest and src cannot be null */
 char *Str_copy(char *dest, const char *src);
 
-/* Appends string src to the end of dest and returns pointer to dest.*/
+/* Appends string src to the end of dest and returns pointer to dest. 
+dest and src cannot be null */
 char *Str_concat(char *dest, const char *src);
 
 /* Compares strings s1 and s2. Returns 0 if they match or the difference 
-of the first set of characters if they don't. Returns error if s1 and s2
-point to null */
+of the first set of characters if they don't. s1 and s2 can't be null */
 int Str_compare(const char *s1, const char *s2);
 
 /* Finds the first occurance of string needle in string haystack. 
 Returns pointer to the location of the occurance, or NULL if there is
 none. If needle is 0 characters long, returns pointer to the first
-char of haystack. Returns error if needle and haystack point to Null. */ 
+char of haystack. Haystack and needle cannot be null. */ 
 char *Str_search(const char *haystack, const char *needle);
 
 #endif
